506abd02bab8ca116c58f453bd73184e140ff07a
Gustavo Oliveira <gustavo@handcom.com.br>
2021-10-31 15:40:49 -0300

    add Brands and Type do Controller Product

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -11,7 +11,7 @@ namespace API.Controllers;
 public class ProductsController : ControllerBase
 {
 
-  private IProductRepository _repository;
+  private readonly IProductRepository _repository;
 
   public ProductsController(IProductRepository repository)
   {
@@ -23,7 +23,7 @@ public class ProductsController : ControllerBase
   public async Task<ActionResult<List<Product>>> GetProducts()
   {
 
-    var products = await _repository.GetProductAsync();
+    var products = await _repository.GetProductsAsync();
 
     return Ok(products);
   }
@@ -33,6 +33,20 @@ public class ProductsController : ControllerBase
   {
     var product = await _repository.GetProductByIdAsync(id);
     return Ok(product);
+  }
+
+  [HttpGet("brands")]
+  public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+  {
+    var productBrands = await _repository.GetProductBrandsAsync();
+    return Ok(productBrands);
+  }
+
+  [HttpGet("types")]
+  public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductTypes()
+  {
+    var productTypes = await _repository.GetProductTypesAsync();
+    return Ok(productTypes);
   }
 
 }
--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -8,9 +8,13 @@ namespace Core.Interfaces;
 
 public interface IProductRepository
 {
-  Task<Product> GetProductByIdAsync(int id);
+  Task<Product?> GetProductByIdAsync(int id);
 
-  Task<IReadOnlyList<Product>> GetProductAsync();
+  Task<IReadOnlyList<Product>> GetProductsAsync();
+
+  Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+
+  Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
 
 }
 
